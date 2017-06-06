cc6d53749b6d60790d143bc7e5b4e9cfd9242a2a
Yumei Tung <yumei.tung@teamaol.com>
2017-06-06 15:37:42 -0400

    remove IInjectBookKeeper, IInjectDoneFileHandler, and IInjectFileManager

--- core/src/main/java/com/aol/advertising/qiao/injector/IInjectDoneFileHandler.java
+++
@@ -1,8 +0,0 @@
-package com.aol.advertising.qiao.injector;
-
-import com.aol.advertising.qiao.injector.file.DoneFileHandler;
-
-public interface IInjectDoneFileHandler
-{
-    public void setDoneFileHandler(DoneFileHandler doneFileHandler);
-}
--- core/src/main/java/com/aol/advertising/qiao/injector/IInjectFileManager.java
+++
@@ -1,8 +0,0 @@
-package com.aol.advertising.qiao.injector;
-
-import com.aol.advertising.qiao.injector.file.watcher.QiaoFileManager;
-
-public interface IInjectFileManager
-{
-    public void setFileManager(QiaoFileManager fileManager);
-}
